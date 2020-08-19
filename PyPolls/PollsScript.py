1f0803db2ecbc63ae0c35566c4a8a67e82df974d
coreymcculley <corey.mcculley@gmail.com>
2020-08-19 16:12:01 -0500

    Added all calculations, comments, and reporting

--- PyPolls/PollsScript.py
+++ PyPolls/PollsScript.py
@@ -1,8 +1,6 @@
 #import modules
 import os
 import csv
-import numpy as np
-
 #load up database
 pollcsvpath = os.path.join( 'Resources', 'election_data.csv')
 #initialize variables
@@ -14,6 +12,7 @@ c2=0
 c3=0
 c4=0
 c_total=0
+winner=""
 #open database
 with open(pollcsvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -25,6 +24,7 @@ for i in range(1,length):
     county.append(data[i][1])
     candidate.append(data[i][2])
 length=len(candidate)
+#Add total vote counts and votes for each candidate
 for i in range(0,length):
     c_total+=1
     if candidate[i] == "Khan":
@@ -35,13 +35,34 @@ for i in range(0,length):
         c3+=1
     if candidate[i] == "O'Tooley":
         c4+=1
+#Calculate percentages for each candidate
 per_c1=round(100*c1/c_total,3)
 per_c2=round(100*c2/c_total,3)
 per_c3=round(100*c3/c_total,3)
 per_c4=round(100*c4/c_total,3)
-
-print(c_total)
-print(per_c1)
-print(per_c2)
-print(per_c3)
-print(per_c4)
+percents=[per_c1,per_c2,per_c3,per_c4]
+#Determine winner
+if per_c1 == max(percents):
+    winner="Khan"
+if per_c2 == max(percents):
+    winner="Correy"
+if per_c3 == max(percents):
+    winner="Li"
+if per_c4 == max(percents):
+    winner="O'Tooley"
+#Print out results and create Txt file
+results = f"""Election Results
+------------------------------------
+Total Votes: {c_total}
+------------------------------------
+Khan: {per_c1}% ({c1})
+Correy: {per_c2}% ({c2})
+Li: {per_c3}% ({c3})
+O'Tooley: {per_c4}% ({c4})
+------------------------------------
+Winner: {winner}"""
+print (results)
+export_results = ''.join(results)
+txtpath = os.path.join ( "Resources", "PyPoll_Summary_Results.txt")
+with open (txtpath,'w') as file: 
+   file.write(export_results)
